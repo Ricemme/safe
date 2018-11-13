cab2bbef45df2cefbedb441529ce9b92445be144
Ricemme <yueaisb78291@163.com>
2018-11-13 09:47:30 +0800

    delete

--- geg/frame/mvc/controller/demo/hello.go
+++
@@ -1,9 +0,0 @@
-package demo
-
-import "gitee.com/johng/gf/g/net/ghttp"
-
-func init() {
-    ghttp.GetServer().BindHandler("/", func(r *ghttp.Request){
-        r.Response.Write("Hello World!")
-    })
-}
--- version.go
+++
@@ -1,5 +0,0 @@
-package gf
-
-const VERSION  = "v1.1.994"
-const AUTHORS  = "john<john@johng.cn>"
-
